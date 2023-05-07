8bc9b24229e81226d8b4b1c3e8e7800a943f1352
Andrii Spiian <andrey.spiyan@icloud.com>
2023-05-07 17:55:42 +0300

    complete

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function generateKey(length, characters) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
